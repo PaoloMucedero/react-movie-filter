c4ccce8d0da5dcd729060e353edccc127172be7a
PaoloMucedero <p.mucedero@gmail.com>
2026-01-28 18:55:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import movieArray from './assets/movieArray'
+import './App.css'
+
+function App() {
+
+  return (
+      <h1>react movie filter</h1>
+            
+  )
+}
+
+export default App
---
+++ src/assets/movieArray.js
@@ -0,0 +1,11 @@
+const movieArray = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+];
+console.log(movieArray);
+
+export default movieArray
