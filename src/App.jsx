8fba7e00ae0f46b29bdd4ab16c2ecfc792714aff
PaoloMucedero <p.mucedero@gmail.com>
2026-01-28 21:29:25 +0100

    Impostata logica e testato funzionamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import movieArray from './assets/movieArray'
 import './App.css'
 
 function App() {
 
+  // stato per il genere selezionato
+  const [selectedGenre, setSelectedGenre] = useState("");
+
+  // stato per i film da mostrare
+  const [filteredMovies, setFilteredMovies] = useState(movieArray);
+
+  // useEffect che gestisce il filtro
+  useEffect(() => {
+    // se non è selezionato nessun genere
+    if (selectedGenre === "") {
+      setFilteredMovies(movieArray);
+    } else {
+      const filtered = movieArray.filter(
+        movie => movie.genre === selectedGenre
+      );
+      setFilteredMovies(filtered);
+    }
+  }, [selectedGenre]);
+
   return (
-      <h1>react movie filter</h1>
-            
+    <div>
+      <h1>React Movie Filter</h1>
+
+      {/* select per scegliere il genere */}
+      <select
+        value={selectedGenre}
+        onChange={(e) => setSelectedGenre(e.target.value)}
+      >
+        <option value="">Tutti i generi</option>
+        <option value="Fantascienza">Fantascienza</option>
+        <option value="Thriller">Thriller</option>
+        <option value="Romantico">Romantico</option>
+        <option value="Azione">Azione</option>
+      </select>
+
+      {/* lista dei film */}
+      <ul>
+        {filteredMovies.map((movie, index) => (
+          <li key={index}>
+            {movie.title}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
